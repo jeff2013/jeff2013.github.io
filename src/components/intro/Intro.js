e22f098d4eb47a58f9618348db4743233ad1cf65
Jeff Chang <jeffreychang2010@gmail.com>
2019-12-27 16:39:46 -0800

    Change repos

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Intro.scss'
+import '../../index.scss'
+
+function Intro() {
+    return (
+        <div className='intro-container'>
+            <p>Jeff Chang</p>
+        </div>
+    )
+}
+
+export default Intro;
