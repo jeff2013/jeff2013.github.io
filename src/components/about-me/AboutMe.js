72fbe66bbadfb0407fa39dd4ec3856e0520648f1
Jeff Chang <jeffreychang2010@gmail.com>
2019-12-28 16:53:28 -0800

    Add images and adjust styles

--- src/components/about-me/AboutMe.js
+++ src/components/about-me/AboutMe.js
@@ -5,13 +5,19 @@ import aboutme from '../../assets/about-me.JPG';
 
 function AboutMe() {
     return (
-        <div className="fullscreen-container">
+        <div className="fullscreen-container about-me">
             <div className="content-container">
                 <img className="about-image" src={aboutme} alt="about-me"></img>
                 <div className="center-container">
                     <div className="content">
                         <h2 className="title">About me</h2>
-                        <p>Hey this is a bit about me!</p>
+                        <p className="description">I strongly believe that the best way to get to know someone is over a good old, bitter, cup of dark black coffee ☕️ so here goes nothing.
+                            <br/>
+                            <br/>
+                            I'm an aspiring project/product manager who has fallen, sort of, in love with software development and beautiful, functional user interfaces. Why sort of?
+                            I love what I do but I still dream about opening a car repair shop, coaching basketball and adopting as many dogs as possible (which I believe is 2 per day).
+                            Keep on scrolling to get a glimpse into my life, what my hobbies, passions, values and If you'd like to chat, you can reach me here!
+                        </p>
                     </div>
                 </div>
             </div>
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,22 +5,19 @@ import AboutMe from '../about-me/AboutMe';
 import Photography from '../photography/Photography';
 import Contact from '../contact/Contact';
 
+function generatePhotosTo(num) {
+  var images = [];
+  for (var i = 1; i <= num; i++) {
+    images.push({
+      id: `photo-${i}`,
+      image: require(`../../assets/photo-${i}.jpg`)
+    })
+  }
+  return images;
+}
+
 function App() {
-
-  const images = [
-    {
-      id: 'photo-1',
-      image: require('../../assets/photo-1.jpg')
-    },
-    {
-      id: 'photo-2',
-      image: require('../../assets/photo-2.jpg')
-    },
-    {
-      id: 'photo-3',
-      image: require('../../assets/photo-3.jpg')
-    }]
-
+  const images = generatePhotosTo(15);
   return (
     <div className="App">
       <Intro/>
