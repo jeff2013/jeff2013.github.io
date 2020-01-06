71b34f9e3e4048362d5de8c95baf3e4da5d03ff9
Jeff Chang <jeffreychang2010@gmail.com>
2020-01-05 21:46:58 -0800

    Update into page
    *  Add intro page descriptions and update styling.

--- src/components/intro/Intro.js
+++ src/components/intro/Intro.js
@@ -2,10 +2,17 @@ import React from 'react'
 import './Intro.scss'
 import '../../index.scss'
 
+
 function Intro() {
     return (
         <div className='intro-container'>
-            <p>Jeff Chang</p>
+            <p className="title">JEFF CHANG</p>
+            <div className="description">
+                <p>Software Developer</p>
+                <p>Basketball Enthusiast</p>
+                <p>Photographer</p>
+                <p>Dog Lover</p>
+            </div>
         </div>
     )
 }
